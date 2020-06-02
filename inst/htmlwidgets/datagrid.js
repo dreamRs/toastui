fe4e4c7a8c97d68e499217be734a596b74329d43
pvictor <perrier.victor@gmail.com>
2020-06-02 10:11:12 +0200

    test button renderer

--- inst/htmlwidgets/datagrid.js
+++ inst/htmlwidgets/datagrid.js
@@ -62,6 +62,46 @@ class CustomBarRenderer {
   }
 }
 
+
+class CustomButtonRenderer {
+  constructor(props) {
+    const el = document.createElement("button");
+    const width = props.columnInfo.renderer.options.width;
+    const status = props.columnInfo.renderer.options.status;
+    el.type = "button";
+    el.style.width = width;
+    el.classList.add("btn");
+    el.classList.add("btn-sm");
+    el.classList.add("btn-" + status);
+
+    this.el = el;
+    this.render(props);
+  }
+
+  getElement() {
+    return this.el;
+  }
+
+  render(props) {
+    var label;
+    if (props.columnInfo.renderer.options.hasOwnProperty("label")) {
+      label = props.columnInfo.renderer.options.label;
+    } else {
+      label = String(props.value);
+    }
+    if (props.columnInfo.renderer.options.hasOwnProperty("icon")) {
+      label = props.columnInfo.renderer.options.icon + " " + label;
+    }
+    const inputId = props.columnInfo.renderer.options.inputId;
+    this.el.onclick = function() {
+      if (HTMLWidgets.shinyMode) {
+        Shiny.setInputValue(inputId, String(props.value));
+      }
+    };
+    this.el.innerHTML = label;
+  }
+}
+
 // HTMLWidgets bindings
 
 HTMLWidgets.widget({
