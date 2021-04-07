57f953321c07db01ef22b937f83ea7054b7726d2
pvictor <perrier.victor@gmail.com>
2021-04-07 15:18:39 +0200

    init charts

---
+++ R/chart.R
@@ -0,0 +1,46 @@
+
+chart <- function(data,
+                  mapping = NULL, 
+                  type = c("column", "bar", "area", "line",
+                           "scatter", "bubble", "boxPlot", 
+                           "heatmap", "treemap", "radialBar", "pie"),
+                  ...,
+                  options = list(),
+                  height = NULL, 
+                  width = NULL, 
+                  elementId = NULL) {
+  type <- match.arg(type)
+  if (!is.list(options))
+    stop("options must be a list")
+  if (is.null(height))
+    options$chart$height <- "auto"
+  if (is.null(width))
+    options$chart$width <- "auto"
+  createWidget(
+    name = "tui-chart",
+    x = list(
+      config = list(
+        type = paste0(type, "Chart"),
+        data = data,
+        options = options
+      )
+    ),
+    width = width,
+    height = height,
+    package = "toastui",
+    elementId = elementId,
+    sizingPolicy = sizingPolicy(
+      defaultWidth = "100%",
+      defaultHeight = "100%",
+      viewer.defaultHeight = "100%",
+      viewer.defaultWidth = "100%",
+      knitr.figure = FALSE,
+      knitr.defaultWidth = "100%",
+      knitr.defaultHeight = "350px",
+      browser.fill = TRUE,
+      viewer.suppress = FALSE,
+      browser.external = TRUE,
+      padding = 0
+    )
+  )
+}
---
+++ inst/htmlwidgets/tui-chart.js
@@ -0,0 +1,41 @@
+/*!
+ * HTMLwidget for TOAST UI Chart
+ * @author Victor Perrier
+ */
+
+/*jshint
+  esversion: 6
+*/
+/*global tui, HTMLWidgets, Shiny */
+
+HTMLWidgets.widget({
+  name: "tui-chart",
+
+  type: "output",
+
+  factory: function(el, width, height) {
+    var chart;
+
+    return {
+      renderValue: function(x) {
+        var type = x.config.type;
+        var data = x.config.data;
+        var options = x.config.options;
+        chart = toastui.Chart[type]({ el, data, options });
+      },
+
+      getWidget: function() {
+        return chart;
+      },
+
+      resize: function(width, height) {
+        /*
+        chart.updateOptions({
+          chart: {width: width, height: height}
+        });
+        */
+      }
+    };
+  }
+});
+
