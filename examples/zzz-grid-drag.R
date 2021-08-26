5e49a84c7eda8629b42bf568138f6af55ee90369
pvictor <perrier.victor@gmail.com>
2021-08-26 11:06:22 +0200

    drag example

---
+++ examples/zzz-grid-drag.R
@@ -0,0 +1,45 @@
+
+library(shiny)
+library(toastui)
+
+ui <- fluidPage(
+  tags$h2("Drag row example"),
+  fluidRow(
+    column(
+      width = 8,
+      datagridOutput("grid")
+    ),
+    column(
+      width = 4,
+      tags$b("Drag:"),
+      verbatimTextOutput("drag"),
+      tags$b("Drop:"),
+      verbatimTextOutput("drop"),
+      tags$b("Data:"),
+      verbatimTextOutput("data")
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  
+  output$grid <- renderDatagrid({
+    datagrid(list(
+      "Variable 1" = 1:10,
+      "Varaible 2" = LETTERS[1:10]
+    ), draggable = TRUE)
+  })
+  
+  output$drag <- renderPrint({
+    input$grid_drag
+  })
+  output$drop <- renderPrint({
+    input$grid_drop
+  })
+  output$data <- renderPrint({
+    input$grid_data
+  })
+  
+}
+
+shinyApp(ui, server)
