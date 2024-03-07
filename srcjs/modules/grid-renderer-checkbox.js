03be6a46527a667da611ff5e83af17d97cf21fc6
pvictor <perrier.victor@gmail.com>
2024-03-07 15:49:52 +0100

    DataGridCheckBoxRenderer

---
+++ srcjs/modules/grid-renderer-checkbox.js
@@ -0,0 +1,32 @@
+class DatagridCheckboxRenderer {
+  constructor(props) {
+    const el = document.createElement("div");
+    el.className = "form-check form-switch d-flex justify-content-center my-1";
+    const input = document.createElement("input");
+    const { grid, rowKey, columnInfo } = props;
+    const checked = Boolean(props.value);
+    input.type = "checkbox";
+    input.className = "form-check-input";
+    input.style.cursor = "pointer";
+    input.checked = checked;
+    input.addEventListener("change", () => {
+      if (input.checked) {
+        grid.setValue(rowKey, columnInfo.name, "TRUE");
+      } else {
+        grid.setValue(rowKey, columnInfo.name, "FALSE");
+      }
+    });
+    el.appendChild(input);
+    this.el = el;
+    this.render(props);
+  }
+  
+  getElement() {
+    return this.el;
+  }
+  
+  render(props) {
+    //const checked = Boolean(props.value);
+    //this.el.checked = checked;
+  }
+}
